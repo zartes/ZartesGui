24605974573d90b9361af6a2fe04a23bc73b155f
jbolea <40656981+jbolea@users.noreply.github.com>
2018-07-13 12:54:46 +0200

    13/07/2018 Midday
    Some changes related to include a property viewer.

--- GeneralFcn/Circuit.m
+++ GeneralFcn/Circuit.m
@@ -13,15 +13,41 @@ classdef Circuit
     
     methods        
         function obj = Constructor(obj)
-            obj.Rf = 10000;
-            obj.Rpar = 2.035e-05;
-            obj.Rn = 0.0232;
-            obj.Rsh = 0.002;
-            obj.invMf = 66;
-            obj.invMin = 24.1;
-            obj.L = 7.7e-08;
-            obj.mN = 650.7;
-            obj.mS = 8133;
+            obj.Rf = PhysicalMeasurement;            
+            obj.Rf.Value = 10000;
+            obj.Rf.Units = '';
+            
+            obj.Rpar = PhysicalMeasurement;            
+            obj.Rpar.Value = 2.035e-05;
+            obj.Rpar.Units = '';
+            
+            obj.Rn = PhysicalMeasurement;
+            obj.Rn.Value = 0.0232;
+            obj.Rn.Units = '';
+            
+            obj.Rsh = PhysicalMeasurement;
+            obj.Rsh.Value = 0.002;
+            obj.Rsh.Units = '';
+            
+            obj.invMf = PhysicalMeasurement;
+            obj.invMf.Value = 66;
+            obj.invMf.Units = '';
+            
+            obj.invMin = PhysicalMeasurement;
+            obj.invMin.Value = 24.1;
+            obj.invMin.Units = '';
+            
+            obj.L = PhysicalMeasurement;
+            obj.L.Value = 7.7e-08;
+            obj.L.Units = '';
+            
+            obj.mN = PhysicalMeasurement;
+            obj.mN.Value = 650.7;
+            obj.mN.Units = '';
+            
+            obj.mS = PhysicalMeasurement;
+            obj.mS.Value = 8133;
+            obj.mS.Units = '';
         end
     end
     
