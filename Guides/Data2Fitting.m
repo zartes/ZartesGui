d5f3bdbed39d775f944a62ef0009017940c8dc9c
zartes <zartes@gmail.com>
2018-08-20 14:03:12 +0200

    20/08/2018 Graphical representation added
    Some tools for graphical representation about the stored data is added.

---
+++ Guides/Data2Fitting.m
@@ -0,0 +1,10 @@
+function Data2Fitting(src,evnt)
+    
+Rect = src.UserData.Rect;
+Datos = src.UserData.Data;
+
+% La idea es acotar los datos que hay dentro del rectangulo para hacer un
+% fitting lineal y obtener la pendiente de la recta.
+
+
+end
