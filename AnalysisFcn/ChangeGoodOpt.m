391e9428b4df0e22a93d2b1a1ba8b4fccf413cf5
zartes <zartes@gmail.com>
2020-01-23 19:48:21 +0100

    Updating I-V Curve filtrage
    Protocol for filtering I-V curves was reviewed and updated.

--- AnalysisFcn/ChangeGoodOpt.m
+++ AnalysisFcn/ChangeGoodOpt.m
@@ -17,14 +17,26 @@ switch sel_typ
         cmenu = uicontextmenu('Visible','on');
         c1 = uimenu(cmenu,'Label',src.Tag);
         try
+            
             data.NumFile = NumFile;
             data.fig.hObject = src.Parent.Parent;
-            data.IVset = data.fig.hObject.UserData;
+            data.IVset = data.fig.hObject.UserData;            
             data.src = src;
-            uimenu(c1,'Label','Remove from analysis','Callback',...
+            data.color = distinguishable_colors(length(data.IVset));
+            if data.IVset(data.NumFile).good
+                uimenu(c1,'Label','Remove for analysis','Callback',...
+                    {@ActionFcn},'UserData',data);
+            else
+                uimenu(c1,'Label','Restore for analysis','Callback',...
+                    {@ActionFcn},'UserData',data);
+            end
+            uimenu(cmenu,'Label','Remove all for analysis','Callback',...
+                {@ActionFcn},'UserData',data);
+            uimenu(cmenu,'Label','Restore all for analysis','Callback',...
                 {@ActionFcn},'UserData',data);
         catch
         end
+        
         
         set(src,'uicontextmenu',cmenu);
     otherwise
@@ -35,7 +47,7 @@ function ActionFcn(src,evnt)
 UserData = get(src,'UserData');
 str = get(src,'Label');
 switch str
-    case 'Remove from analysis'
+    case 'Remove for analysis'
         
         h = findobj(src.Parent.Parent.Parent.Children,'Type','Line');
         Label = src.Parent.Label;
@@ -46,6 +58,33 @@ switch str
         end
         
         UserData.IVset(UserData.NumFile).good = 0;
+        UserData.fig.hObject.UserData = UserData.IVset;
+        
+    case 'Remove all for analysis'
+        h = findobj(src.Parent.Parent.Parent.Children,'Type','Line');
+        for i = 1:length(h)
+            UserData.IVset(i).good = 0;
+            h(i).Color = [0.8 0.8 0.8];
+        end
+        UserData.fig.hObject.UserData = UserData.IVset;
+    case 'Restore all for analysis'
+        h = findobj(src.Parent.Parent.Parent.Children,'Type','Line');
+        color = distinguishable_colors(length(h));
+        for i = 1:length(h)
+            UserData.IVset(i).good = 1;
+            h(i).Color = color(length(h)-i+1,:);
+        end
+        UserData.fig.hObject.UserData = UserData.IVset;
+    case 'Restore for analysis'
+        h = findobj(src.Parent.Parent.Parent.Children,'Type','Line');
+        Label = src.Parent.Label;
+        h1 = findobj(h,'Tag',Label);
+        for i = 1:length(h1)
+            h1(i).Color = UserData.color(UserData.NumFile,:);
+%             h1(i).Visible = 'off';
+        end
+        
+        UserData.IVset(UserData.NumFile).good = 1;
         UserData.fig.hObject.UserData = UserData.IVset;
         
     otherwise
